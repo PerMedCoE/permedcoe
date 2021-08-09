e4d1f14ce279f793d0db6ef3c1d36dae2930c73f
fconejer <francisco.conejero@bsc.es>
2021-08-09 12:46:51 +0200

    Added installed_files.txt register to install in supercomputer script.

--- install_sc.sh
+++ install_sc.sh
@@ -10,6 +10,6 @@ path="$1/lib/python${version}/site-packages/"
 mkdir -p ${path}
 
 export PYTHONPATH=${path}:$PYTHONPATH
-python3 setup.py install --prefix=$1
+python3 setup.py install --prefix=$1 --record installed_files.txt
 
 echo "----- Installation finished -----"
