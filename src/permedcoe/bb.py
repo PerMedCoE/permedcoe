aa9b9378bd39ff40995d074d67512721ef9d619f
fconejer <francisco.conejero@bsc.es>
2022-12-16 15:58:03 +0100

    Moved COMPUTING_UNITS from container to permedcoe package.

--- src/permedcoe/bb.py
+++ src/permedcoe/bb.py
@@ -6,13 +6,31 @@ import os
 def get_container_path():
     """Retrieve the PerMedCoE container images path.
 
+    :return: The value of the PERMEDCOE_IMAGES environment variable.
     :raises Exception: If the "PERMEDCOE_IMAGES" is not defined.
     """
     # Container definition environment variable
-    if "PERMEDCOE_IMAGES" in os.environ:
-        return os.environ["PERMEDCOE_IMAGES"]
+    CONTAINER_PATH_VN = "PERMEDCOE_IMAGES"
+    if CONTAINER_PATH_VN in os.environ:
+        return os.environ[CONTAINER_PATH_VN]
     else:
-        raise Exception("Please define PERMEDCOE_IMAGES environment variable with the path.")
+        raise Exception("Please define %s environment variable with the path." % CONTAINER_PATH_VN)
 
 
 CONTAINER_PATH = get_container_path()
+
+
+def get_computing_units():
+    """Retrieve the computing units required for the tasks.
+
+    :return: The value of the COMPUTING_UNITS environment variable as integer.
+    """
+    # Computing units for the tasks
+    COMPUTING_UNITS_VN = "COMPUTING_UNITS"
+    if COMPUTING_UNITS_VN in os.environ:
+        return int(os.environ[COMPUTING_UNITS_VN])
+    else:
+        return 1
+
+
+COMPUTING_UNITS = get_computing_units()
